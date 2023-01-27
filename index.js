6ba0e72c1d0e3deea17eaa32db99e82750099842
Jackson Dean <jckson.dean@gmail.com>
2023-01-26 21:26:51 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const bip39 = require('bip39');
+const EC = require('elliptic').ec;
+const secp = require('@noble/secp256k1');
+const bs58check = require('bs58check');
+const assert = require('assert');
+
+const seedHex = 'caf797541983d115976714b8fb33720439e0790a6186106016d427c128b4e7e35e7f9dd8e2b0b8ccd08a50de574f9b6233e52fcec32a5bcd8e005e38f5dcd572';
+
+// elliptic key pair
+const ec = new EC('secp256k1');
+const ecKeys = ec.keyFromPrivate(seedHex, 'hex');
+const publicKey = ecKeys.getPublic('hex');
+
+// secp256k1 key pair
+const privkey = secp.utils.hashToPrivateKey(seedHex);
+const privkeyHex = secp.utils.bytesToHex(privkey);
+const pubkkey = secp.getPublicKey(privkey);
+
+console.log('elliptic priv key hex: ', ecKeys.getPrivate('hex'));
+console.log('secp256k priv key hex: ', privkeyHex);
+console.log('ellpitic pub key hex : ', publicKey);
+console.log('secp256k pub key hex : ', secp.utils.bytesToHex(pubkkey));
+
+
