351da08273895491d96b4c7e8542d002d9f16784
Jackson Dean <jckson.dean@gmail.com>
2023-01-27 09:04:39 -0800

    fix how we're getting the secp priv key

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const EC = require("elliptic").ec;
 const secp = require("@noble/secp256k1");
 
-const seedHex =
-  "caf797541983d115976714b8fb33720439e0790a6186106016d427c128b4e7e35e7f9dd8e2b0b8ccd08a50de574f9b6233e52fcec32a5bcd8e005e38f5dcd572";
+  const seedHex =
+    "caf797541983d115976714b8fb33720439e0790a6186106016d427c128b4e7e35e7f9dd8e2b0b8ccd08a50de574f9b6233e52fcec32a5bcd8e005e38f5dcd572";
 
-// elliptic key pair
-const ec = new EC("secp256k1");
-const ecKeys = ec.keyFromPrivate(seedHex);
-const publicKey = ecKeys.getPublic("hex");
+  // elliptic key pair
+  const ec = new EC("secp256k1");
+  const ecKeys = ec.keyFromPrivate(seedHex);
+  const publicKey = ecKeys.getPublic("hex");
 
-// secp256k1 key pair
-const privkey = secp.utils.hashToPrivateKey(seedHex);
-const privkeyHex = secp.utils.bytesToHex(privkey);
-const pubkkey = secp.getPublicKey(privkey);
+  // secp256k1 key pair
+  const secpPrivKey = secp.utils.mod(BigInt(`0x${seedHex}`), secp.CURVE.n);
+  const pubkkey = secp.getPublicKey(secpPrivKey);
 
-console.log("elliptic priv key hex: ", ecKeys.getPrivate("hex"));
-console.log("secp256k priv key hex: ", privkeyHex);
-console.log("ellpitic pub key hex : ", publicKey);
-console.log("secp256k pub key hex : ", secp.utils.bytesToHex(pubkkey));
+  console.log("elliptic priv key hex: ", ecKeys.getPrivate("hex"));
+  console.log("secp256k priv key hex: ", secpPrivKey.toString(16));
+
+  console.log("ellpitic pub key hex : ", publicKey);
+  console.log("secp256k pub key hex : ", secp.utils.bytesToHex(pubkkey));
+
